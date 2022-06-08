6f3a290c956f57dd8da683254a93a3f803730af1
zcq100 <zcq100@gmail.com>
2022-06-08 15:26:56 +0800

    Update 汉化gui

--- script.js
+++ script.js
@@ -56,16 +56,16 @@ googleLink.addEventListener('click', function (e) {
 var canvas = document.getElementsByTagName('canvas')[0];
 resizeCanvas();
 
-var config = {
+let config = {
     SIM_RESOLUTION: 128,
     DYE_RESOLUTION: 1024,
-    CAPTURE_RESOLUTION: 512,
-    DENSITY_DISSIPATION: 1,
-    VELOCITY_DISSIPATION: 0.2,
+    CAPTURE_RESOLUTION: 1024,
+    DENSITY_DISSIPATION: 1.5,
+    VELOCITY_DISSIPATION: 0.3,
     PRESSURE: 0.8,
     PRESSURE_ITERATIONS: 20,
     CURL: 30,
-    SPLAT_RADIUS: 0.25,
+    SPLAT_RADIUS: 0.29,
     SPLAT_FORCE: 6000,
     SHADING: true,
     COLORFUL: true,
@@ -73,11 +73,11 @@ var config = {
     PAUSED: false,
     BACK_COLOR: { r: 0, g: 0, b: 0 },
     TRANSPARENT: false,
-    BLOOM: true,
+    BLOOM: false,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
-    BLOOM_INTENSITY: 0.8,
-    BLOOM_THRESHOLD: 0.6,
+    BLOOM_INTENSITY: 0.3,
+    BLOOM_THRESHOLD: 0.41,
     BLOOM_SOFT_KNEE: 0.7,
     SUNRAYS: true,
     SUNRAYS_RESOLUTION: 196,
@@ -209,77 +209,77 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 
 function startGUI () {
     var gui = new dat.GUI({ width: 300 });
-    gui.add(config, 'DYE_RESOLUTION', { 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality').onFinishChange(initFramebuffers);
-    gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256 }).name('sim resolution').onFinishChange(initFramebuffers);
-    gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('density diffusion');
-    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('velocity diffusion');
-    gui.add(config, 'PRESSURE', 0.0, 1.0).name('pressure');
-    gui.add(config, 'CURL', 0, 50).name('vorticity').step(1);
-    gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('splat radius');
-    gui.add(config, 'SHADING').name('shading').onFinishChange(updateKeywords);
-    gui.add(config, 'COLORFUL').name('colorful');
-    gui.add(config, 'PAUSED').name('paused').listen();
+    gui.add(config, 'DYE_RESOLUTION', { '非常高': 2048, '较高': 1024, '中等': 512, '较低': 256, '非常低': 128 }).name('画质').onFinishChange(initFramebuffers);
+    gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256, '1024': 1024 }).name('模拟分辨率').onFinishChange(initFramebuffers);
+    gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('密度扩散');
+    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('速度扩散');
+    gui.add(config, 'PRESSURE', 0.0, 1.0).name('压力');
+    gui.add(config, 'CURL', 0, 50).name('涡度').step(1);
+    gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('飞溅半径');
+    gui.add(config, 'SHADING').name('着色').onFinishChange(updateKeywords);
+    gui.add(config, 'COLORFUL').name('色彩');
+    gui.add(config, 'PAUSED').name('暂停').listen();
 
-    gui.add({ fun: function () {
+    gui.add({ fun: () => {
         splatStack.push(parseInt(Math.random() * 20) + 5);
-    } }, 'fun').name('Random splats');
+    } }, 'fun').name('随机 splats');
 
-    var bloomFolder = gui.addFolder('Bloom');
-    bloomFolder.add(config, 'BLOOM').name('enabled').onFinishChange(updateKeywords);
+    let bloomFolder = gui.addFolder('Bloom');
+    bloomFolder.add(config, 'BLOOM').name('启用').onFinishChange(updateKeywords);
     bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 2.0).name('intensity');
     bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 1.0).name('threshold');
 
-    var sunraysFolder = gui.addFolder('Sunrays');
-    sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
-    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
+    let sunraysFolder = gui.addFolder('☀光照');
+    sunraysFolder.add(config, 'SUNRAYS').name('启用').onFinishChange(updateKeywords);
+    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('强度');
 
-    var captureFolder = gui.addFolder('Capture');
-    captureFolder.addColor(config, 'BACK_COLOR').name('background color');
-    captureFolder.add(config, 'TRANSPARENT').name('transparent');
-    captureFolder.add({ fun: captureScreenshot }, 'fun').name('take screenshot');
+    let captureFolder = gui.addFolder('🚀截图');
+    captureFolder.addColor(config, 'BACK_COLOR').name('背景色');
+    captureFolder.add(config, 'TRANSPARENT').name('透明');
+    captureFolder.add({ fun: captureScreenshot }, 'fun').name('📷截图');
 
-    var github = gui.add({ fun : function () {
-        window.open('https://github.com/PavelDoGreat/WebGL-Fluid-Simulation');
-        ga('send', 'event', 'link button', 'github');
-    } }, 'fun').name('Github');
-    github.__li.className = 'cr function bigFont';
-    github.__li.style.borderLeft = '3px solid #8C8C8C';
-    var githubIcon = document.createElement('span');
-    github.domElement.parentElement.appendChild(githubIcon);
-    githubIcon.className = 'icon github';
+    // let github = gui.add({ fun : () => {
+    //     window.open('https://github.com/PavelDoGreat/WebGL-Fluid-Simulation');
+    //     ga('send', 'event', 'link button', 'github');
+    // } }, 'fun').name('Github');
+    // github.__li.className = 'cr function bigFont';
+    // github.__li.style.borderLeft = '3px solid #8C8C8C';
+    // let githubIcon = document.createElement('span');
+    // github.domElement.parentElement.appendChild(githubIcon);
+    // githubIcon.className = 'icon github';
 
-    var twitter = gui.add({ fun : function () {
-        ga('send', 'event', 'link button', 'twitter');
-        window.open('https://twitter.com/PavelDoGreat');
-    } }, 'fun').name('Twitter');
-    twitter.__li.className = 'cr function bigFont';
-    twitter.__li.style.borderLeft = '3px solid #8C8C8C';
-    var twitterIcon = document.createElement('span');
-    twitter.domElement.parentElement.appendChild(twitterIcon);
-    twitterIcon.className = 'icon twitter';
+    // let twitter = gui.add({ fun : () => {
+    //     ga('send', 'event', 'link button', 'twitter');
+    //     window.open('https://twitter.com/PavelDoGreat');
+    // } }, 'fun').name('Twitter');
+    // twitter.__li.className = 'cr function bigFont';
+    // twitter.__li.style.borderLeft = '3px solid #8C8C8C';
+    // let twitterIcon = document.createElement('span');
+    // twitter.domElement.parentElement.appendChild(twitterIcon);
+    // twitterIcon.className = 'icon twitter';
 
-    var discord = gui.add({ fun : function () {
-        ga('send', 'event', 'link button', 'discord');
-        window.open('https://discordapp.com/invite/CeqZDDE');
-    } }, 'fun').name('Discord');
-    discord.__li.className = 'cr function bigFont';
-    discord.__li.style.borderLeft = '3px solid #8C8C8C';
-    var discordIcon = document.createElement('span');
-    discord.domElement.parentElement.appendChild(discordIcon);
-    discordIcon.className = 'icon discord';
+    // let discord = gui.add({ fun : () => {
+    //     ga('send', 'event', 'link button', 'discord');
+    //     window.open('https://discordapp.com/invite/CeqZDDE');
+    // } }, 'fun').name('Discord');
+    // discord.__li.className = 'cr function bigFont';
+    // discord.__li.style.borderLeft = '3px solid #8C8C8C';
+    // let discordIcon = document.createElement('span');
+    // discord.domElement.parentElement.appendChild(discordIcon);
+    // discordIcon.className = 'icon discord';
 
-    var app = gui.add({ fun : function () {
-        ga('send', 'event', 'link button', 'app');
-        window.open('http://onelink.to/5b58bn');
-    } }, 'fun').name('Check out mobile app');
-    app.__li.className = 'cr function appBigFont';
-    app.__li.style.borderLeft = '3px solid #00FF7F';
-    var appIcon = document.createElement('span');
-    app.domElement.parentElement.appendChild(appIcon);
-    appIcon.className = 'icon app';
+    // let app = gui.add({ fun : () => {
+    //     ga('send', 'event', 'link button', 'app');
+    //     window.open('http://onelink.to/5b58bn');
+    // } }, 'fun').name('Check out mobile app');
+    // app.__li.className = 'cr function appBigFont';
+    // app.__li.style.borderLeft = '3px solid #00FF7F';
+    // let appIcon = document.createElement('span');
+    // app.domElement.parentElement.appendChild(appIcon);
+    // appIcon.className = 'icon app';
 
-    if (isMobile())
-        { gui.close(); }
+    // if (isMobile())
+    gui.close();
 }
 
 function isMobile () {
